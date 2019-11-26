37bfba374db3c61c34cdc132b658e83bbceace6d
young-zy <youngzhaozhou@gmail.com>
2019-11-26 13:03:10 +0800

    add ButtonClick Logic

--- app/src/main/java/cf/youngauthentic/aboutme/MainActivity.kt
+++ app/src/main/java/cf/youngauthentic/aboutme/MainActivity.kt
@@ -1,12 +1,33 @@
 package cf.youngauthentic.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        done_button.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+        nickname_text.text = nickname_edit.text
+        nickname_edit.visibility = View.GONE
+        view.visibility = View.GONE
+        nickname_text.visibility = View.VISIBLE
+
+        //Hide the KeyBoard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+
     }
 }
