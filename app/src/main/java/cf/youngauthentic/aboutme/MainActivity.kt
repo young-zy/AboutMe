fc95f842c2670e05cd33189821e36914751d8e16
young-zy <youngzhaozhou@gmail.com>
2019-11-26 13:29:56 +0800

    add Data Binding

--- app/src/main/java/cf/youngauthentic/aboutme/MainActivity.kt
+++ app/src/main/java/cf/youngauthentic/aboutme/MainActivity.kt
@@ -1,33 +1,42 @@
 package cf.youngauthentic.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ContextMenu
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import cf.youngauthentic.aboutme.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        done_button.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
-    private fun addNickname(view: View){
-        nickname_text.text = nickname_edit.text
-        nickname_edit.visibility = View.GONE
-        view.visibility = View.GONE
-        nickname_text.visibility = View.VISIBLE
+    private fun addNickname(view: View) {
+
+        binding.apply {
+            nicknameText.text = nickname_edit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         //Hide the KeyBoard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken,0)
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
 }
